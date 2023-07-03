a7f423017cf31425f8c8ed499180c790b84a6d74
MarlonHersal <hern.sal.dz@gmail.com>
2023-07-03 07:26:37 -0600

    Add project files.

---
+++ APIFurnitureStore.Share/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIFurnitureStore.Share
+{
+    public class Order
+    {
+        public int id { get; set; }
+        public int OrderNumber { get; set; }
+        public int ClientId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
