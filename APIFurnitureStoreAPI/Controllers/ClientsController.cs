f54631a50b51cd91d348599479a2f8d0a2c92eef
EmpresaTesla <EmpresaTesla@bitbucket.org>
2023-07-05 17:47:16 -0600

    Controlador clientes

---
+++ APIFurnitureStoreAPI/Controllers/ClientsController.cs
@@ -0,0 +1,57 @@
+﻿using APIFurnitureStore.Data;
+using APIFurnitureStore.Share;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIFurnitureStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly APIFurnitureStoreContext _context;
+        public ClientsController(APIFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {   
+            var lista = await _context.Clients.ToListAsync();
+            return Ok(lista);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var client = await _context.Clients.FirstOrDefaultAsync(c => c.Id == id);
+            if(client == null) return NotFound();
+            return Ok(client);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Client client)
+        {
+            await _context.Clients.AddAsync(client);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Post", client.Id, client);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Client client)
+        {
+            _context.Clients.Update(client);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Client client)
+        {
+            if (client == null) return NotFound();
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
